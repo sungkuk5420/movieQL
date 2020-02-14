c6d5a74a68e9f041027b0959a647074084cdd724
Sungkuk5420 <sungkuk5420@gmail.com>
2020-02-14 22:22:56 +0900

    아이디를 사용한 유저 탐색 쿼리

---
+++ graphql/db.js
@@ -0,0 +1,31 @@
+export const people = [
+  {
+    id: 1,
+    name: "Nicolas",
+    age: 18,
+    gender: "female"
+  },
+  {
+    id: 2,
+    name: "Kim",
+    age: 22,
+    gender: "female"
+  },
+  {
+    id: 3,
+    name: "An",
+    age: 27,
+    gender: "male"
+  },
+  {
+    id: 4,
+    name: "BOBO",
+    age: 2,
+    gender: "male"
+  }
+];
+
+export const getById = id => {
+  const filteredPeople = people.filter(person => id === people.id);
+  return filteredPeople[0];
+};
