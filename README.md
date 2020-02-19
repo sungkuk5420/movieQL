8b4712e5aaebb933cb324c4b200f7234622f464c
Sungkuk5420 <sungkuk5420@gmail.com>
2020-02-19 22:25:41 +0900

    REST API 를 래핑해서 사용하기 YTS API

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 ###
+
 GraphQL 첫 코스 연습
 
-
 ### graphql-yoga
+
 npm i graphql-yoga
 
 ### 그래프큐엘 장점
-앤드 포인트를 줄일 수 있다 
+
+앤드 포인트를 줄일 수 있다
 ex) /users/ GET에서 이름 , 성별, 나이 등을 보낼거고 사용하지 않을 영역까지 다 보내버리는데 그래프큐엘에선 필요한 필드만 호출가능.
 Over-fetcing없이 코드를 짤 수 있다고 한다.
 
@@ -15,27 +17,31 @@ Over-fetcing없이 코드를 짤 수 있다고 한다.
 /user/1/
 페이스북 기준 앱을 시작하려고 3가지 요청을 해야함 이것이 바로 Under-fetching없이 코딩이 가능하다는 점.
 
-예를들어 
+예를들어
 query {
-    feed {
-        comments
-        likeNumber
-    }
-    notifications {
-        isRead
-    }
-    user {
-        username
-        profilePic
-    }
+feed {
+comments
+likeNumber
+}
+notifications {
+isRead
+}
+user {
+username
+profilePic
+}
 }
 
+### nodemon
 
-###  nodemon
 npm i -g nodemon
 
-
 ### babel-node
+
 npm i -g babel-cli
 npm i --save-dev @babel/core @babel/node
 npm i babel-cli babel-preset-env babel-preset-stage-3 --dev
+
+### node-fetch
+
+npm i node-fetch
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,15 +1,27 @@
-import { getMovies, getById, addMovie, deleteMovie } from "./db";
+// import { getMovies, getById, addMovie, deleteMovie } from "./db";
+
+// const resolvers = {
+//   Query: {
+//     movies: () => getMovies(),
+//     movie: (_, { id }) => {
+//       return getById(id);
+//     }
+//   },
+//   Mutation: {
+//     addMovie: (_, { name, score }) => addMovie(name, score),
+//     deleteMovie: (_, { id }) => deleteMovie(id)
+//   }
+// };
+
+// export default resolvers;
+
+import { getMovies, getMoviesDetails, getMoviesSuggestions } from "./db";
 
 const resolvers = {
   Query: {
-    movies: () => getMovies(),
-    movie: (_, { id }) => {
-      return getById(id);
-    }
-  },
-  Mutation: {
-    addMovie: (_, { name, score }) => addMovie(name, score),
-    deleteMovie: (_, { id }) => deleteMovie(id)
+    movies: (_, { limit, rating }) => getMovies(limit, rating),
+    movieDetails: (_, { id }) => getMoviesDetails(id),
+    moviesSuggestions: (_, { id }) => getMoviesSuggestions(id)
   }
 };
 
